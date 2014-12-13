ae9649a2b9eba3e5ff92f4eaa9b82bc3016b12c6
swellergis <shaun.weller@gmail.com>
2014-12-13 09:07:56 -0700

    [refactor] Modify Location to include constants for coordinate min thresholds.

--- src/main/java/com/swellergis/geoicon/domain/Location.java
+++ src/main/java/com/swellergis/geoicon/domain/Location.java
@@ -8,14 +8,25 @@ package com.swellergis.geoicon.domain;
  */
 public class Location {
 	private static final String DEF_COORDINATE = "0";
+	private static final int THRESHOLD_MIN_X = -180;
+	private static final int THRESHOLD_MIN_Y = -90;
 	private double x;
 	private double y;
 	private double z;
 	
-	// default constructor
+	/**
+	 * default constructor
+	 */
 	public Location() {
 		this(DEF_COORDINATE, DEF_COORDINATE, DEF_COORDINATE);
 	}
+	/**
+	 * Location constructor accepting longitude (x), latitude (y), and elevation (z) parameters.
+	 * 
+	 * @param x
+	 * @param y
+	 * @param z
+	 */
 	public Location(String x, String y, String z) {
 		verifyCoordinateInput(x, y, z);
 		this.x = Double.parseDouble(x);
@@ -26,12 +37,12 @@ public class Location {
 	// verify client input for coordinate values are valid
 	private void verifyCoordinateInput(String sx, String sy, String sz) {
 		double dx = Double.parseDouble(sx);
-		if (dx < -180) {
+		if (dx < THRESHOLD_MIN_X) {
 			throw new IllegalArgumentException("Longitude (x) cannot be < -180. Input value: " + sx);
 		}
 
 		double dy = Double.parseDouble(sy);
-		if (dy < -90) {
+		if (dy < THRESHOLD_MIN_Y) {
 			throw new IllegalArgumentException("Latitude (y) cannot be < -90. Input value: " + sy);
 		}
 	}
@@ -44,7 +55,7 @@ public class Location {
 	public double getY() {
 		return y;
 	}
-	// altitude
+	// elevation
 	public double getZ() {
 		return z;
 	}
