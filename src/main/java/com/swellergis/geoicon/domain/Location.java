49b8da1e26a7515fc15b125798cbecacb6907406
swellergis <shaun.weller@gmail.com>
2014-12-13 07:22:33 -0700

    Add Location class.

---
+++ src/main/java/com/swellergis/geoicon/domain/Location.java
@@ -0,0 +1,18 @@
+package com.swellergis.geoicon.domain;
+
+public class Location {
+	private double x;
+	private double y;
+	private double z;
+	
+	public double getX() {
+		return x;
+	}
+	public double getY() {
+		return y;
+	}
+	public double getZ() {
+		return z;
+	}
+
+}
